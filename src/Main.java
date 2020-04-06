daa9331f86f6b2a1e57fd2daa60e5b3faf175db2
Valeria-Kuzina <58110393+Valeria-Kuzina@users.noreply.github.com>
2020-04-06 20:30:24 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String args[]) {
         // TODO: в переменную refill ввести сумму пополнения счёта
         double refill = 100;
-        int bonus = refill >= 1000 ? (int) (refill / 100) : 0;
+        int bonus = refill > 1000 ? (int) (refill / 100) : 0;
         double balance = 100 + refill + bonus;
         System.out.print("Итоговый счёт(в рублях) = " + balance);
     }
